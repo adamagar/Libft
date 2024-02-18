c97a9c74a3550f3cd073e9ebb2accf4a9f731950
Adam Agar <adamagar88@gmail.com>
2024-02-18 13:45:40 -0500

    Bug fixes

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,9 +6,11 @@
 /*   By: aagar <aagar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 15:03:07 by aagar             #+#    #+#             */
-/*   Updated: 2024/02/17 15:35:36 by aagar            ###   ########.fr       */
+/*   Updated: 2024/02/18 13:44:37 by aagar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,9 +6,11 @@
 /*   By: aagar <aagar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 15:43:13 by aagar             #+#    #+#             */
-/*   Updated: 2024/02/17 17:05:44 by aagar            ###   ########.fr       */
+/*   Updated: 2024/02/18 13:43:57 by aagar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
