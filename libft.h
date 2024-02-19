3d8653818b6535330c33f7a911b464e90c44459a
Adam Agar <adamagar88@gmail.com>
2024-02-19 14:08:43 -0500

    Implement memcmp function

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aagar <aagar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 16:48:07 by aagar             #+#    #+#             */
-/*   Updated: 2024/02/19 13:47:56 by aagar            ###   ########.fr       */
+/*   Updated: 2024/02/19 13:59:19 by aagar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,5 @@ char	*ft_strrchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 #endif
