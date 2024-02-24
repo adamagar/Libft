890ffcfcae9947c648743ebb2250c6e61f61504b
Adam Agar <adamagar88@gmail.com>
2024-02-24 16:06:30 -0500

    Implement substr function

---
+++ ft_substr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aagar <aagar@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/24 15:03:28 by aagar             #+#    #+#             */
+/*   Updated: 2024/02/24 15:50:52 by aagar            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	s_len;
+
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+	{
+		substr = (char *)malloc(1);
+		if (!substr)
+			return (NULL);
+		substr[0] = '\0';
+	}
+	else
+	{
+		if (len > s_len - start)
+			len = s_len - start;
+		substr = (char *)malloc(len + 1);
+		if (!substr)
+			return (NULL);
+		ft_strlcpy(substr, s + start, len + 1);
+	}
+	return (substr);
+}
