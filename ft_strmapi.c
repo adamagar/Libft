96e922679cfb38ba646af512d6de15463e080acd
Adam Agar <adamagar88@gmail.com>
2024-03-09 13:37:06 -0500

    Implement strmapi function

---
+++ ft_strmapi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aagar <aagar@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/09 13:27:40 by aagar             #+#    #+#             */
+/*   Updated: 2024/03/09 13:36:24 by aagar            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*new_str;
+	unsigned int	i;
+
+	if (!s || !f)
+		return (NULL);
+	new_str = malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!new_str)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		new_str[i] = f(i, s[i]);
+		i++;
+	}
+	new_str[i] = '\0';
+	return (new_str);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aagar <aagar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 16:48:07 by aagar             #+#    #+#             */
-/*   Updated: 2024/03/07 13:33:51 by aagar            ###   ########.fr       */
+/*   Updated: 2024/03/09 13:36:43 by aagar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,4 +45,5 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	ft_strmapi(char const *s, char (*f)(unsigned int, char));
 #endif
